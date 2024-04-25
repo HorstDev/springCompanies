5c9a113ded967d55cc0d405c1c4490d99fda0867
HorstDev <gorstsergei@mail.ru>
2024-04-25 22:22:29 +0400

    init project

---
+++ src/test/java/com/example/springCompanies/SpringCompaniesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springCompanies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCompaniesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
